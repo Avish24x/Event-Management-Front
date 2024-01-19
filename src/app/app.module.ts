73c95206c5c3002c5cbd19fcd624d06b20c7ec79
avish24x <mileshtaukoorah@gmail.com>
2024-01-19 10:54:19 +0400

    implemented Search bar on Events and did events card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
@@ -26,6 +26,7 @@ import { AnalyticsChartsComponent } from './components/analytics-charts/analytic
 import {HighchartsChartModule} from 'highcharts-angular';
 import { EventDisplayComponent } from './components/event-display/event-display.component';
 import { CommunicationComponent } from './components/communication/communication.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 
 @NgModule({
@@ -48,14 +49,16 @@ import { CommunicationComponent } from './components/communication/communication
     FinanceComponent,
     AnalyticsChartsComponent,
     EventDisplayComponent,
-    CommunicationComponent
+    CommunicationComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
     HighchartsChartModule,
-    FullCalendarModule
+    FullCalendarModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
