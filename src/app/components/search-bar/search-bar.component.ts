e09c3f8a75502481f6ac054ba86e0c5d654c8a09
Abdurrahman Gurib <abdurrahman.gurib@sdworx.com>
2024-01-20 17:56:40 +0400

    Resolve search-bar.component.ts scss file path issues

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -3,7 +3,8 @@ import { Component, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.css']
+  styleUrls: ['./search-bar.component.scss']
+
 })
 export class SearchBarComponent {
   searchQuery: string = '';
