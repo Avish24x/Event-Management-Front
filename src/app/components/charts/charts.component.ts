159dd714d8cd48ac1651762c0e6ede90ff858c36
Abdurrahman Gurib <abdurrahman.gurib@sdworx.com>
2024-01-24 00:26:40 +0400

    Implementation of 4 different charts in Analytics Components

--- src/app/components/charts/charts.component.ts
+++ src/app/components/charts/charts.component.ts
@@ -9,9 +9,12 @@ import * as Highcharts from 'highcharts';
 export class ChartsComponent implements AfterViewInit {
   @ViewChild('highchartsChart') private highchartsChartRef!: ElementRef;
 
-  Highcharts: typeof Highcharts = Highcharts; // required
-  chartConstructor: string = 'chart'; // optional string, defaults to 'chart'
-  chartOptions: Highcharts.Options = {
+  Highcharts: typeof Highcharts = Highcharts;
+  chartConstructor: string = 'chart';
+  updateFlag: boolean = false;
+
+  // Bar Chart
+  barChartOptions: Highcharts.Options = {
     chart: {
       type: 'bar',
     },
@@ -27,27 +30,102 @@ export class ChartsComponent implements AfterViewInit {
       }
     },
     series: [{
-      name: 'Highcharts Bar Chart',
+      name: 'Bar Chart',
+      type: 'bar', // Specify the chart type
       data: [10, 20, 30]
-    }] as Highcharts.SeriesOptionsType[], // Explicitly cast the series property
-    plotOptions: {
-      series: {
-        animation: {
-          duration: 1000, // Set the duration of the animation in milliseconds
-          easing: 'easeOutBounce' // Set the easing function for the animation
-        }
-      }
-    }
-  }; // required
-
-  chartCallback: Highcharts.ChartCallbackFunction = function (chart) {
-    // optional function, defaults to null
-    // You can add any additional logic or actions here
+    }]
   };
 
-  updateFlag: boolean = false; // optional boolean
-  oneToOneFlag: boolean = true; // optional boolean, defaults to false
-  runOutsideAngular: boolean = false; // optional boolean, defaults to false
+  // Line Chart
+  lineChartOptions: Highcharts.Options = {
+    chart: {
+      type: 'line',
+    },
+    title: {
+      text: 'Highcharts Line Chart'
+    },
+    xAxis: {
+      categories: ['Label 1', 'Label 2', 'Label 3']
+    },
+    yAxis: {
+      title: {
+        text: 'Value'
+      }
+    },
+    series: [{
+      name: 'Line Chart',
+      type: 'line', // Specify the chart type
+      data: [30, 20, 10]
+    }]
+  };
+
+  // Area Chart
+  areaChartOptions: Highcharts.Options = {
+    chart: {
+      type: 'area',
+    },
+    title: {
+      text: 'Highcharts Area Chart'
+    },
+    xAxis: {
+      categories: ['Label 1', 'Label 2', 'Label 3']
+    },
+    yAxis: {
+      title: {
+        text: 'Value'
+      }
+    },
+    series: [{
+      name: 'Area Chart',
+      type: 'area', // Specify the chart type
+      data: [5, 15, 25]
+    }]
+  };
+
+  // Pie Chart
+  pieChartOptions: Highcharts.Options = {
+    chart: {
+      type: 'pie',
+    },
+    title: {
+      text: 'Highcharts Pie Chart'
+    },
+    series: [{
+      name: 'Pie Chart',
+      type: 'pie', // Specify the chart type
+      data: [
+        ['Category 1', 30],
+        ['Category 2', 40],
+        ['Category 3', 20],
+        ['Category 4', 10]
+      ]
+    }]
+  };
+
+  // Column Chart
+  columnChartOptions: Highcharts.Options = {
+    chart: {
+      type: 'column',
+    },
+    title: {
+      text: 'Highcharts Column Chart'
+    },
+    xAxis: {
+      categories: ['Label 1', 'Label 2', 'Label 3']
+    },
+    yAxis: {
+      title: {
+        text: 'Value'
+      }
+    },
+    series: [{
+      name: 'Column Chart',
+      type: 'column', // Specify the chart type
+      data: [25, 15, 30]
+    }]
+  };
+
+  constructor() {}
 
   ngAfterViewInit() {
     this.createHighchartsChart();
