e5d18b21d8662f1ef48676567420e1d93febbafe
Abdurrahman Gurib <abdurrahman.gurib@sdworx.com>
2024-01-27 00:24:00 +0400

    Implementation of Group Leader Page

--- src/app/components/group-leaders/group-leaders.component.ts
+++ src/app/components/group-leaders/group-leaders.component.ts
@@ -1,3 +1,5 @@
+// group-leaders.component.ts
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +8,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./group-leaders.component.scss']
 })
 export class GroupLeadersComponent {
+  leaders = [
+    {
+      name: 'Leader 1',
+      photo: '../../../assets/images/spark/03.png',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod.',
+      expanded: false
+    },
+    {
+      name: 'Leader 2',
+      photo: '../../../assets/images/spark/03.png',
+      bio: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.',
+      expanded: false
+    },
+    // Add more leaders as needed
+    {
+      name: 'Leader 2',
+      photo: '../../../assets/images/spark/03.png',
+      bio: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.',
+      expanded: false
+    },
+    {
+      name: 'Leader 2',
+      photo: '../../../assets/images/spark/03.png',
+      bio: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.',
+      expanded: false
+    },
 
+    {
+      name: 'Leader 2',
+      photo: '../../../assets/images/spark/03.png',
+      bio: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.',
+      expanded: false
+    },
+
+    {
+      name: 'Leader 2',
+      photo: '../../../assets/images/spark/03.png',
+      bio: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.',
+      expanded: false
+    },
+  ];
+
+  expandBio(leader: any): void {
+    leader.expanded = !leader.expanded;
+  }
 }
