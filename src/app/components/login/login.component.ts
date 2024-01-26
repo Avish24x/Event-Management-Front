598c95f16384abd2b7f5479cbff0ef2b269492bb
Yeshan Sahody <yeshan.sahody@sdworx.com>
2024-01-26 09:06:07 +0400

    OTP FORM

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 
+declare var $: any;
+
 // Custom validator function
 export function containsSpecialCharacter(control: AbstractControl): { [key: string]: boolean } | null {
   // Add your desired special characters to the regular expression
@@ -20,20 +22,29 @@ export function containsSpecialCharacter(control: AbstractControl): { [key: stri
 })
 export class LoginComponent {
   loginForm: FormGroup;
+  otpForm: FormGroup;
 
   constructor(private fb: FormBuilder) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(8), containsSpecialCharacter]]
     });
+
+    this.otpForm = this.fb.group({
+      otp: ['', [Validators.required]]
+    });
   }
 
   onSubmit() {
     if (this.loginForm.valid) {
-      console.log('Form submitted:', this.loginForm.value);
+      $('#otpModal').modal('show');
     } else {
       console.log('Form has validation errors');
       this.loginForm.get('password')?.markAsTouched();
     }
+  }
+
+  verifyOTP() {
+    console.log('OTP verification successful!');
   }
 }
