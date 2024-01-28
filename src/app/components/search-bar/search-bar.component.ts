28abd97853424260c1ee366a6f8cd1488f637905
avish24x <mileshtaukoorah@gmail.com>
2024-01-28 16:49:19 +0400

    search bar fix

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,5 +1,5 @@
 import { Component, Output, EventEmitter } from '@angular/core';
-
+ 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
@@ -10,30 +10,30 @@ export class SearchBarComponent {
   isClosed: boolean = true;
   isExpanded: boolean = false;
   isProfileDropdownOpen: boolean = false; // Add this line
-
+ 
   @Output() searchEvent = new EventEmitter<string>();
-
+ 
   search() {
     this.searchEvent.emit(this.searchQuery);
   }
-
+ 
   scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }
-
+ 
   // Toggle Chatbox
   toggleChatbox() {
     this.isClosed = !this.isClosed;
   }
-
+ 
   onClose() {
     this.isClosed = true;
   }
-
+ 
   onClick() {
     this.isExpanded = !this.isExpanded;
   }
-
+ 
   // Toggle Profile Dropdown
   toggleProfileDropdown() {
     this.isProfileDropdownOpen = !this.isProfileDropdownOpen;
