385acf2f3ff36877ac85ab1a39fb7e17c24d2c40
Yeshan Sahody <yeshan.sahody@sdworx.com>
2024-02-14 13:14:26 +0400

    ApiConnection

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,46 @@
+import { Injectable, inject } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+//import { JwtHelperService } from '@auth0/angular-jwt';
+import { Router } from '@angular/router';
+import { StorageService } from 'src/app/services/storage.service';
+import { UserResponse } from '../../app/model/user-response';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+ // private jwtHelperService = inject(JwtHelperService);
+  private router = inject(Router);
+  //private storageService = inject(StorageService)
+  private loggedInSubject = new BehaviorSubject<boolean>(false);
+
+  login(result: { [key: string]: any }): void {
+    
+   // const decodedToken = this.jwtHelperService.decodeToken(result['token']);
+   // if (decodedToken) {
+      //const userObject: UserResponse = {
+        
+     // };
+      //this.storageService.set('token', result['token']);
+     // this.storageService.set('user', userObject);
+      //if (userObject.role as string === 'Administrator') {
+        this.router.navigate(['/admin-dashboard']);
+        this.loggedInSubject.next(true);
+      }
+     // else {
+      //  this.router.navigate(['/home']);
+      //  this.loggedInSubject.next(true);
+     // }
+    }
+ // }
+ // logout(): void {
+  //  this.storageService.clear();
+  //  this.loggedInSubject.next(false);
+  //  this.router.navigate(['/']);
+
+ // }
+
+  //getUserLoginStatus(): Observable<boolean> {
+  //  return this.loggedInSubject.asObservable();
+  //}
+//}
